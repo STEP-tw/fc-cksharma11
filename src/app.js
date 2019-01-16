a8947da849982b381854a118c323d8f13102ad6a
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-16 14:04:34 +0530

    Added functions to send data and not found status code

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
 const fs = require('fs');
 
+const displayLog = function(req, res) {
+  console.log(req.method, req.url);
+};
+
 const getFileName = function(url) {
   if (url == '/') return './index.html';
   return '.' + url;
 };
 
+const send = function(res, data) {
+  res.write(data);
+  res.end();
+};
+
+const sendNotFound = function(res) {
+  res.statusCode = 404;
+  res.end();
+};
+
 const app = (req, res) => {
+  displayLog(req, res);
   const path = getFileName(req.url);
   fs.readFile(path, (err, data) => {
-    if (!err) {
-      res.write(data);
-      res.end();
-    }
-    res.statusCode = 404;
-    res.end();
+    if (!err) return send(res, data);
+    return sendNotFound(res);
   });
 };
-// Export a function that can act as a handler
 
 module.exports = app;

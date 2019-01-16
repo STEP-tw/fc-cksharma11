b57f41d4dec57c0747476ce43db921fc09761a27
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-16 13:56:12 +0530

    Added index.html
    Features :
    Title “Flower Catalog” at the top
    Image of flowers
    Links to pages of Abeliophyllum, Ageratum and a guest book.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+const fs = require('fs');
+
+const getFileName = function(url) {
+  if (url == '/') return './index.html';
+  return '.' + url;
 };
 
+const app = (req, res) => {
+  const path = getFileName(req.url);
+  fs.readFile(path, (err, data) => {
+    if (!err) {
+      res.write(data);
+      res.end();
+    }
+    res.statusCode = 404;
+    res.end();
+  });
+};
 // Export a function that can act as a handler
 
 module.exports = app;

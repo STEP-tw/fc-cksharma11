27c3dd5d16db4ed7d3eb4014fe1684268f5df997
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-21 21:19:06 +0530

    Added jsdoc documentations

--- src/express.js
+++ src/express.js
@@ -1,9 +1,22 @@
+/**
+ *
+ * for validating user's request
+ * @param {Request} req
+ * @param {Response} route
+ */
+
 const isMatching = (req, route) => {
   if (route.method && req.method != route.method) return false;
   if (route.url instanceof RegExp && route.url.test(req.url)) return true;
   if (route.url && req.url != route.url) return false;
   return true;
 };
+
+/**
+ * Represents Express class.
+ * It's a HTTP request handler framework that will help to deal with user requests
+ * @constructor
+ */
 
 class Express {
   constructor() {
@@ -14,6 +27,9 @@ class Express {
     let matchingRoutes = this.routes.filter(route => isMatching(req, route));
     let remaining = [...matchingRoutes];
 
+    /**
+     * next is a utility for express class that will allow to deal with mutiple valid requests
+     */
     let next = () => {
       let current = remaining[0];
       if (!current) return;
--- src/serverUtils.js
+++ src/serverUtils.js
@@ -2,8 +2,8 @@ const parseCommentDetails = function(comment) {
   const commentObject = new Object();
   const decodedComment = decodeURIComponent(comment).replace(/\+/g, ' ');
   const [, author, , commentText] = decodedComment.split(/=|&/);
-  commentObject.name = author;
-  commentObject.comment = commentText;
+  commentObject.name = unescape(author);
+  commentObject.comment = unescape(commentText);
   return commentObject;
 };
 
